1c85d14852e740496bc6437d8ae4503159b082c0
Emi Blazco <103598648+EmilianoBlazco@users.noreply.github.com>
2024-02-20 16:22:14 -0300

    Aplicacion de estilo al formulario de registro
    Co-authored-by: Leandro Radins <radins97@gmail.com>

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -3,7 +3,6 @@ import RegisterForm from "@components/RegisterForm";
 const RegisterPage = () => {
   return (
     <>
-      <h1>Register Page</h1>
       <RegisterForm />
     </>
   );
--- app/components/RegisterForm.tsx
+++ app/components/RegisterForm.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface FormFields {
@@ -33,14 +32,131 @@ const RegisterForm = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit(onsubmit)}>
-        <input type="text" {...register("name")} />
-        <input type="text" {...register("surname")} />
-        <input type="text" {...register("dni")} />
-        <input type="email" {...register("email")} />
-        <input type="password" {...register("password")} />
-        <button type="submit">Submit</button>
-      </form>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-28 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          {/* <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          /> */}
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Registrarse
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-4" onSubmit={handleSubmit(onsubmit)}>
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-1xl font-medium leading-6 text-gray-900"
+              >
+                Nombre
+              </label>
+              <div className="mt-1">
+                <input
+                  id="name"
+                  type="text"
+                  {...register("name")}
+                  //autoComplete="name"
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="surname"
+                className="block text-1xl font-medium leading-6 text-gray-900"
+              >
+                Apellido
+              </label>
+              <div className="mt-1">
+                <input
+                  id="surname"
+                  type="text"
+                  {...register("surname")}
+                  //autoComplete="family-name" // Fix: Use "family-name" as the value for the autoComplete attribute
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="dni"
+                className="block text-1xl font-medium leading-6 text-gray-900"
+              >
+                DNI
+              </label>
+              <div className="mt-1">
+                <input
+                  id="dni"
+                  type="number"
+                  {...register("dni")}
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-1xl font-medium leading-6 text-gray-900"
+              >
+                Correo Electrónico
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  type="email"
+                  {...register("email")}
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-1xl font-medium leading-6 text-gray-900"
+                >
+                  Contraseña
+                </label>
+              </div>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  type="password"
+                  {...register("password")}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md mt-8 bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Registrarse
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Ya tiene una cuenta?{" "}
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Inicia sesión
+            </a>
+          </p>
+        </div>
+      </div>
     </>
   );
 };
